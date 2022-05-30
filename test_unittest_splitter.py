6213611fc0adb780df171bbf62c2b9a602ee9983
AlekseyGolikov <golikov.aleksey.1987@yandex.ru>
2022-05-30 13:15:09 +0300

    update test_unittest_splitter.py

--- test_unittest_splitter.py
+++ test_unittest_splitter.py
@@ -5,24 +5,30 @@ import time
 
 def timer(func):
     def wrapper(*args):
-        start = time.perf_counter()
+        start = time.perf_counter_ns()
         func(*args)
-        runtime = time.perf_counter() - start
-        print("Время работы тестового API: %4.2f" % runtime)
+        runtime = (time.perf_counter_ns() - start) / 1000000
+        print("Время работы теста %s: %1.5f мс" % (func.__name__, runtime))
     return wrapper
 
-@timer
+
 class TestSplitFunction(unittest.TestCase):
     def setUp(self):
         pass # Выполнить настройку тестов при необходимости
     def tearDown(self) -> None:
         pass # Выполнить завершающие действия при необходимости
+
+    @timer
     def testSimpleString(self):
         r = splitter.split('erger 100 rfg 23.4')
         self.assertEqual(r, ['erger','100','rfg','23.4'])
+
+    @timer
     def testTypeConvert(self):
         r = splitter.split('erger 100 rfg 23.4', [str,int,str,float])
         self.assertEqual(r, ['erger',100,'rfg',23.4])
+
+    @timer
     def testDelimiter(self):
         r = splitter.split('erger,100,rfg,23.4', delimiter=',')
         self.assertEqual(r, ['erger','100','rfg','23.4'])
